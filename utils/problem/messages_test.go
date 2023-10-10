f917b0fc11f18df79ff4c10b06041da402927097
Shouhei Nishi <shohei.nishi.tp@apresiasystems.co.jp>
2023-10-10 14:37:00 +0900

    If argument detail of functions of problem package is empty string, now JSONs marshaled from result of these functions do not include detail field.

---
+++ utils/problem/messages_test.go
@@ -0,0 +1,64 @@
+// Copyright 2023 APRESIA Systems LTD.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package problem_test
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/ShouheiNishi/openapi5g/commondata"
+	"github.com/ShouheiNishi/openapi5g/utils/problem"
+	"github.com/samber/lo"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMessages(t *testing.T) {
+	assert.Equal(t, commondata.ProblemDetails{
+		Status: http.StatusInternalServerError,
+		Title:  lo.ToPtr("Unspecified NF failure"),
+		Cause:  lo.ToPtr("UNSPECIFIED_NF_FAILURE"),
+		Detail: lo.ToPtr("Not implemented"),
+	}, problem.NotImplemented())
+
+	assert.Equal(t, commondata.ProblemDetails{
+		Status: http.StatusInternalServerError,
+		Title:  lo.ToPtr("System failure"),
+		Cause:  lo.ToPtr("SYSTEM_FAILURE"),
+	}, problem.SystemFailure(""))
+
+	assert.Equal(t, commondata.ProblemDetails{
+		Status: http.StatusInternalServerError,
+		Title:  lo.ToPtr("System failure"),
+		Cause:  lo.ToPtr("SYSTEM_FAILURE"),
+		Detail: lo.ToPtr("TEST"),
+	}, problem.SystemFailure("TEST"))
+
+	assert.Equal(t, commondata.ProblemDetails{
+		Status: http.StatusBadRequest,
+		Title:  lo.ToPtr("Mandatory IE missing"),
+		Cause:  lo.ToPtr("MANDATORY_IE_MISSING"),
+		InvalidParams: []commondata.InvalidParam{
+			{
+				Param:  "/foo",
+				Reason: lo.ToPtr("Test"),
+			},
+		},
+	}, problem.MandatoryIEMissing([]commondata.InvalidParam{
+		{
+			Param:  "/foo",
+			Reason: lo.ToPtr("Test"),
+		},
+	}, ""))
+}
